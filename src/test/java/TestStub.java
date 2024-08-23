06ffb7283f0c8e926654b555ebf16363931c008a
Michael Landis <michael@momentohq.com>
2024-08-23 15:14:22 -0700

    test: add stub test to exercise tests

---
+++ src/test/java/TestStub.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestStub {
+  @Test
+  public void testStub() {
+    assertEquals(1, 1);
+  }
+}
