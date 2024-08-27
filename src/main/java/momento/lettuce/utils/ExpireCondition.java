de96f4c6b2c4074fd8a6bdf43ca0b861537852f0
Michael Landis <michael@momentohq.com>
2024-08-27 09:29:14 -0700

    chore: add ExpireCondition to decode ExpireArgs
    Lettuce provides `ExpireArgs` to specify expiry conditions, but does
    not have a helpful way of decoding them. We provide the helper
    `ExpireCondition` to do this.

---
+++ src/main/java/momento/lettuce/utils/ExpireCondition.java
@@ -0,0 +1,46 @@
+package momento.lettuce.utils;
+
+import io.lettuce.core.ExpireArgs;
+import io.lettuce.core.codec.StringCodec;
+import io.lettuce.core.protocol.CommandArgs;
+
+public class ExpireCondition {
+    private final boolean requireExistingExpiry;
+    private final boolean requiresNoExpiry;
+    private final boolean requiresLessThan;
+    private final boolean requiresGreaterThan;
+
+    public ExpireCondition(boolean requiresExistingExpiry, boolean requiresNoExpiry, boolean requiresLessThan, boolean requiresGreaterThan) {
+        this.requireExistingExpiry = requiresExistingExpiry;
+        this.requiresNoExpiry = requiresNoExpiry;
+        this.requiresLessThan = requiresLessThan;
+        this.requiresGreaterThan = requiresGreaterThan;
+    }
+
+    public boolean requiresExistingExpiry() {
+        return requireExistingExpiry;
+    }
+
+    public boolean requiresNoExpiry() {
+        return requiresNoExpiry;
+    }
+
+    public boolean requiresLessThan() {
+        return requiresLessThan;
+    }
+
+    public boolean requiresGreaterThan() {
+        return requiresGreaterThan;
+    }
+
+    public static ExpireCondition fromExpireArgs(ExpireArgs expireArgs) {
+        CommandArgs<String, String> args = new CommandArgs<>(StringCodec.UTF8);
+        expireArgs.build(args);
+        var commandString = args.toCommandString();
+        var requiresExistingExpiry = commandString.contains("XX");
+        var requiresNoExpiry = commandString.contains("NX");
+        var requiresLessThan = commandString.contains("LT");
+        var requiresGreaterThan = commandString.contains("GT");
+        return new ExpireCondition(requiresExistingExpiry, requiresNoExpiry, requiresLessThan, requiresGreaterThan);
+    }
+}
