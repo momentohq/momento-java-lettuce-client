5fbb556bb986b0f1904178c8f7976c9b4f75e849
Michael Landis <malandis@gmail.com>
2024-09-06 12:18:29 -0700

    ci: add publish gradle task and release workflows (#12)
    Adds a gradle publish script and release workflows.

--- src/main/java/momento/lettuce/utils/ValidatorUtils.java
+++ src/main/java/momento/lettuce/utils/ValidatorUtils.java
@@ -1,6 +1,13 @@
 package momento.lettuce.utils;
 
+/** Provides utility methods for validating arguments. */
 public class ValidatorUtils {
+  /**
+   * Ensures that the provided value is within the range of an integer.
+   *
+   * @param value The value to check.
+   * @param argumentName The name of the argument.
+   */
   public static void ensureInIntegerRange(long value, String argumentName) {
     if (value < Integer.MIN_VALUE || value > Integer.MAX_VALUE) {
       throw MomentoToLettuceExceptionMapper.createIntegerOutOfRangeException(argumentName, value);
